0514c588e622a142079588cda9426ace64444ad4
cplagiannakos <80714915+cplagiannakos@users.noreply.github.com>
2025-01-27 12:24:56 -0500

    Create code.r
    created code file

---
+++ code.r
@@ -0,0 +1,19 @@
+# this is my first R script
+# do some things
+x = 34
+y = 16
+z = x + y   # addition
+w = y/x     # division
+# display the results
+x
+y
+z
+w
+# change x
+x = "some text"
+# display the results
+x
+y
+z
+w
+
